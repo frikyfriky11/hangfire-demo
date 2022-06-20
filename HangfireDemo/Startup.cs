bf2fa0ce986c38f8911041a656e5075c78a4195a
Stefano Previato <frikyfriky11@gmail.com>
2022-06-20 08:59:54 +0200

    Setup Hangfire as a service

--- HangfireDemo/Startup.cs
+++ HangfireDemo/Startup.cs
@@ -1,20 +1,48 @@
-﻿using HangfireDemo.Services;
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using HangfireDemo.Services;
 
 namespace HangfireDemo;
 
 public class Startup
 {
+  private readonly IConfiguration _configuration;
+
+  public Startup(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
+  
   public void ConfigureServices(IServiceCollection services)
   {
     services.AddControllers();
 
     services.AddSingleton<IRepository, InMemoryRepository>();
+
+    services.AddHangfire(configuration => configuration
+      .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+      .UseSimpleAssemblyNameTypeSerializer()
+      .UseRecommendedSerializerSettings()
+      .UseSqlServerStorage(_configuration.GetConnectionString("HangfireDb"), new SqlServerStorageOptions()
+      {
+        CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+        SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+        QueuePollInterval = TimeSpan.Zero,
+        UseRecommendedIsolationLevel = true,
+        DisableGlobalLocks = true,
+      }));
+
+    services.AddHangfireServer();
   }
 
   public void Configure(IApplicationBuilder app)
   {
     app.UseRouting();
 
-    app.UseEndpoints(endpoints => endpoints.MapControllers());
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapControllers();
+      endpoints.MapHangfireDashboard();
+    });
   }
 }
