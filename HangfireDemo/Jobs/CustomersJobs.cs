21f9f82e48720553bf1797f8516e378cf7eb3a28
Stefano Previato <frikyfriky11@gmail.com>
2022-06-20 09:00:03 +0200

    Added job examples

---
+++ HangfireDemo/Jobs/CustomersJobs.cs
@@ -0,0 +1,27 @@
+﻿using HangfireDemo.Models;
+using HangfireDemo.Services;
+
+namespace HangfireDemo.Jobs;
+
+public class CustomersJobs
+{
+  private readonly ILogger<CustomersJobs> _logger;
+  private readonly IRepository _repository;
+
+  public CustomersJobs(IRepository repository, ILogger<CustomersJobs> logger)
+  {
+    _repository = repository;
+    _logger = logger;
+  }
+
+  public void SendGoodbyeMail(int customerId)
+  {
+    Customer? customer = _repository.Get(customerId);
+    _logger.LogInformation("Sending goodbye email to customer {CustomerId} with name {CustomerName}", customerId, customer?.Name);
+  }
+
+  public void SendUpdateMail(int customerId, string? customerName)
+  {
+    _logger.LogInformation("Sending update email to customer {CustomerId} with name {CustomerName}", customerId, customerName);
+  }
+}
